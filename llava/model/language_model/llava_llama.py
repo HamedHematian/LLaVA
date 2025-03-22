12937946ddfbe5cab854d6aea88254b429de892c
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-03-22 11:34:04 -0500

    Update llava_llama.py

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -77,6 +77,8 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
+        print('forward')
+
         input_ids, attention_mask, past_key_values, inputs_embeds, labels = self.prepare_inputs_labels_for_multimodal(input_ids, attention_mask, past_key_values, labels, images)
 
         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
