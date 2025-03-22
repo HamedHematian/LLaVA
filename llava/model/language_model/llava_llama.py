cded9d362fb95e1ffef8ec49fb74291f40eec978
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-03-22 11:05:33 -0500

    Update llava_llama.py

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -54,11 +54,11 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
     def get_model(self):
         return self.model
 
-
-        def forward(
+    def forward(
         self,
         input_ids: torch.LongTensor = None,
         attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[List[torch.FloatTensor]] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
         labels: Optional[torch.LongTensor] = None,
@@ -66,147 +66,80 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         images: Optional[torch.FloatTensor] = None,
-        images_cd: Optional[torch.FloatTensor] = None,
-        cd_beta: Optional[torch.FloatTensor] = None,
-        cd_alpha: Optional[torch.FloatTensor] = None,
+        image_sizes: Optional[List[List[int]]] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-        output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-        )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        input_ids, attention_mask, past_key_values, inputs_embeds, labels = self.prepare_inputs_labels_for_multimodal(input_ids, attention_mask, past_key_values, labels, images)
+        if inputs_embeds is None:
+            (
+                input_ids,
+                position_ids,
+                attention_mask,
+                past_key_values,
+                inputs_embeds,
+                labels
+            ) = self.prepare_inputs_labels_for_multimodal(
+                input_ids,
+                position_ids,
+                attention_mask,
+                past_key_values,
+                labels,
+                images,
+                image_sizes
+            )
 
-        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
-        outputs = self.model(
+        return super().forward(
             input_ids=input_ids,
             attention_mask=attention_mask,
+            position_ids=position_ids,
             past_key_values=past_key_values,
             inputs_embeds=inputs_embeds,
+            labels=labels,
             use_cache=use_cache,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict
         )
 
-        hidden_states = outputs[0]
-        logits = self.lm_head(hidden_states)
+    @torch.no_grad()
+    def generate(
+        self,
+        inputs: Optional[torch.Tensor] = None,
+        images: Optional[torch.Tensor] = None,
+        image_sizes: Optional[torch.Tensor] = None,
+        **kwargs,
+    ) -> Union[GenerateOutput, torch.LongTensor]:
+        position_ids = kwargs.pop("position_ids", None)
+        attention_mask = kwargs.pop("attention_mask", None)
+        if "inputs_embeds" in kwargs:
+            raise NotImplementedError("`inputs_embeds` is not supported")
 
-        loss = None
-        if labels is not None:
-            # Shift so that tokens < n predict n
-            shift_logits = logits[..., :-1, :].contiguous()
-            shift_labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
-            shift_logits = shift_logits.view(-1, self.config.vocab_size)
-            shift_labels = shift_labels.view(-1)
-            # Enable model/pipeline parallelism
-            shift_labels = shift_labels.to(shift_logits.device)
-            loss = loss_fct(shift_logits, shift_labels)
+        if images is not None:
+            (
+                inputs,
+                position_ids,
+                attention_mask,
+                _,
+                inputs_embeds,
+                _
+            ) = self.prepare_inputs_labels_for_multimodal(
+                inputs,
+                position_ids,
+                attention_mask,
+                None,
+                None,
+                images,
+                image_sizes=image_sizes
+            )
+        else:
+            inputs_embeds = self.get_model().embed_tokens(inputs)
 
-        if not return_dict:
-            output = (logits,) + outputs[1:]
-            return (loss,) + output if loss is not None else output
-
-        return CausalLMOutputWithPast(
-            loss=loss,
-            logits=logits,
-            past_key_values=outputs.past_key_values,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
+        return super().generate(
+            position_ids=position_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            **kwargs
         )
-
-    
-
-    # def forward(
-    #     self,
-    #     input_ids: torch.LongTensor = None,
-    #     attention_mask: Optional[torch.Tensor] = None,
-    #     position_ids: Optional[torch.LongTensor] = None,
-    #     past_key_values: Optional[List[torch.FloatTensor]] = None,
-    #     inputs_embeds: Optional[torch.FloatTensor] = None,
-    #     labels: Optional[torch.LongTensor] = None,
-    #     use_cache: Optional[bool] = None,
-    #     output_attentions: Optional[bool] = None,
-    #     output_hidden_states: Optional[bool] = None,
-    #     images: Optional[torch.FloatTensor] = None,
-    #     image_sizes: Optional[List[List[int]]] = None,
-    #     return_dict: Optional[bool] = None,
-    # ) -> Union[Tuple, CausalLMOutputWithPast]:
-
-    #     if inputs_embeds is None:
-    #         (
-    #             input_ids,
-    #             position_ids,
-    #             attention_mask,
-    #             past_key_values,
-    #             inputs_embeds,
-    #             labels
-    #         ) = self.prepare_inputs_labels_for_multimodal(
-    #             input_ids,
-    #             position_ids,
-    #             attention_mask,
-    #             past_key_values,
-    #             labels,
-    #             images,
-    #             image_sizes
-    #         )
-
-    #     return super().forward(
-    #         input_ids=input_ids,
-    #         attention_mask=attention_mask,
-    #         position_ids=position_ids,
-    #         past_key_values=past_key_values,
-    #         inputs_embeds=inputs_embeds,
-    #         labels=labels,
-    #         use_cache=use_cache,
-    #         output_attentions=output_attentions,
-    #         output_hidden_states=output_hidden_states,
-    #         return_dict=return_dict
-    #     )
-
-    # @torch.no_grad()
-    # def generate(
-    #     self,
-    #     inputs: Optional[torch.Tensor] = None,
-    #     images: Optional[torch.Tensor] = None,
-    #     image_sizes: Optional[torch.Tensor] = None,
-    #     **kwargs,
-    # ) -> Union[GenerateOutput, torch.LongTensor]:
-    #     position_ids = kwargs.pop("position_ids", None)
-    #     attention_mask = kwargs.pop("attention_mask", None)
-    #     if "inputs_embeds" in kwargs:
-    #         raise NotImplementedError("`inputs_embeds` is not supported")
-
-    #     if images is not None:
-    #         (
-    #             inputs,
-    #             position_ids,
-    #             attention_mask,
-    #             _,
-    #             inputs_embeds,
-    #             _
-    #         ) = self.prepare_inputs_labels_for_multimodal(
-    #             inputs,
-    #             position_ids,
-    #             attention_mask,
-    #             None,
-    #             None,
-    #             images,
-    #             image_sizes=image_sizes
-    #         )
-    #     else:
-    #         inputs_embeds = self.get_model().embed_tokens(inputs)
-
-    #     return super().generate(
-    #         position_ids=position_ids,
-    #         attention_mask=attention_mask,
-    #         inputs_embeds=inputs_embeds,
-    #         **kwargs
-    #     )
 
     def prepare_inputs_for_generation(self, input_ids, past_key_values=None,
                                       inputs_embeds=None, **kwargs):
@@ -220,7 +153,6 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         if image_sizes is not None:
             inputs['image_sizes'] = image_sizes
         return inputs
-
 
     def prepare_inputs_for_generation_cd(self, input_ids, past_key_values=None,
                                       inputs_embeds=None, **kwargs):
